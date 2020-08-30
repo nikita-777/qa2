cbfa9306454646a4cfbf36c146a9789eaf04b21a
Ligita <nikita-777@inbox.lv>
2020-08-30 12:18:47 +0100

    we did something

--- src/test/java/ThirdHomework.java
+++ src/test/java/ThirdHomework.java
@@ -1,4 +1,4 @@
-import org.apache.logging.log4j.core.util.SystemClock;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -9,34 +9,53 @@ import java.util.List;
 
 public class ThirdHomework {
     private final By ARTICLE = By.tagName("article");
-    private final By TITLE = By.xpath(".//h1[contains(class, 'headline__title')]");
+    private final By TITLE = By.xpath(".//h1[contains(@class, 'd-inline')]");
+    private final By COMMENTS_COUNT = By.xpath(".//a[contains(@class,'d=print-none')]");
+
+    private final By ARTICLE_PAGE_TITLE = By.xpath(".//h1[contains(@class, 'headline__title')]");
+    private final By ARTICLE_PAGE_COMMENTS = By.xpath(".//a[contains(@class, 'comment-count')]");
+
+    private final By COMMENTS_PAGE_COUNT = By.xpath(".//span[contains(@class, 'type-cnt')]");
+
+
 
     @Test
     public void thirdArticle() {
-        //Some changes here
-        final String TITLE_TO_FIND = "Выборы в Риге: KNAB начал 10 проверок о возможном нарушении правил агитации"
-
         System.setProperty("webdriver.chrome.driver", "c://chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://rus.delfi.lv");
 
-        //get all articles
         List<WebElement> articles = driver.findElements(ARTICLE);
+        WebElement article = articles.get(2);
+        String homePageTitle = article.findElement(TITLE).getText();
 
-        //find given article by text
-        for (WebElement article : articles){
-
+        int homePageCommentsCount = 0;
+        if (!article.findElements(COMMENTS_COUNT).isEmpty()) {
+            homePageCommentsCount = parseCommentCount(article.findElement(COMMENTS_COUNT).getText());
         }
-        //click on it
+        System.out.println(homePageTitle + " " + homePageCommentsCount);
 
-        //find article title
+        article.findElement(TITLE).click();
+        String articlePageTitle = driver.findElement(ARTICLE_PAGE_TITLE).getText();
+        int articlePageCommentCount = Integer.parseInt(driver.findElement(ARTICLE_PAGE_COMMENTS).getText());
 
-        //check it
+        Assertions.assertTrue(homePageTitle.startsWith(articlePageTitle), "Wrong title!");
+        Assertions.assertEquals(homePageCommentsCount, articlePageCommentCount, "Wrong comment count");
 
-        String text = driver.findElement(By.xpath(".//h1[contains(@class, 'headline__title')]")).getText();
+        driver.findElement(ARTICLE_PAGE_COMMENTS).click();
+        String commentsPageTitle = driver.findElement(ARTICLE_PAGE_COMMENTS).getText();
+        int commentsPageCommentsCount = Integer.parseInt(driver.findElement(COMMENTS_PAGE_COUNT).getText());
 
+        Assertions.assertTrue(homePageTitle.startsWith(commentsPageTitle));
+        Assertions.assertEquals(homePageCommentsCount, commentsPageCommentsCount, "Wrong comment count");
+    }
+
+
+        private int parseCommentCount(String textToParse) {
+            textToParse = textToParse.substring(1, textToParse.length() - 1);
+            return Integer.parseInt(textToParse);
+        }
 
     }
 
-}
